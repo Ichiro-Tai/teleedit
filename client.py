572a5b971bebffa89218b7d766aec532c51e120b
Ichiro <IDTai@hotmail.com>
2019-10-11 16:34:08 -0500

    Initial commit

---
+++ client.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import socket
+
+if (len(sys.argv) - 1 < 1):
+    print("Invalid format")
+    exit(0)
+
+host_ip = sys.argv[1]
+
+UDP_PORT = 5005
+
+msg = "TEST MESSAGE FROM CLIENT"
+
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+
+msg_encoded = msg.encode('utf-8')
+
+sock.sendto(msg_encoded, (host_ip, UDP_PORT))
+sock.sendto(msg_encoded, (host_ip, UDP_PORT))
+sock.sendto(msg_encoded, (host_ip, UDP_PORT))
---
+++ host.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+import socket
+
+clients = []
+
+UDP_IP = "127.0.0.1"
+UDP_PORT = 5005
+
+hostname = socket.gethostname()
+ip_addr = socket.gethostbyname(hostname)
+
+print("Your Computer Name is:" + hostname)
+print("Your Computer IP Address is:" + ip_addr)
+
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+sock.bind((UDP_IP, UDP_PORT))
+
+while True:
+    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    if(data is None):
+        continue
+    print ("received message:", data.decode('utf8'))
