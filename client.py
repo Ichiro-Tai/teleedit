f10b2dd31ed753d250f0ec24166785170c391ef3
Billy Li <zl20@linux-14.ews.illinois.edu>
2019-10-11 17:25:55 -0500

    establish host client communication

--- client.py
+++ client.py
@@ -10,7 +10,8 @@ if (len(sys.argv) - 1 < 1):
 
 host_ip = sys.argv[1]
 
-UDP_PORT = 5005
+HOST_UDP_PORT = 5005
+CLIENT_UDP_PORT = 6006
 
 hostname = socket.gethostname()
 IPAddr = socket.gethostbyname(hostname)
@@ -23,7 +24,15 @@ msg += " " + connect_msg + " " + IPAddr
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
+sock.bind((IPAddr, CLIENT_UDP_PORT))
 
 msg_encoded = msg.encode('utf-8')
 
-sock.sendto(msg_encoded, (host_ip, UDP_PORT))
+sock.sendto(msg_encoded, (host_ip, HOST_UDP_PORT))
+
+while True:
+    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    if(data is None):
+        continue
+    msg = data.decode('utf8')
+    print ("received message:", msg)
--- host.py
+++ host.py
@@ -5,7 +5,8 @@ from util import File
 
 clients = []
 
-UDP_PORT = 5005
+HOST_UDP_PORT = 5005
+CLIENT_UDP_PORT = 6006
 test_file = File('test.txt')
 
 hostname = socket.gethostname()
@@ -16,22 +17,27 @@ print("Your Computer IP Address is:" + ip_addr)
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
-sock.bind((ip_addr, UDP_PORT))
+sock.bind((ip_addr, HOST_UDP_PORT))
 
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
     if(data is None):
         continue
     msg = data.decode('utf8')
-    print ("received message:", msg.decode('utf8'))
+    print ("received message:", msg)
 
-    message_array = str.split(" ", 3)
+    message_array = msg.split(" ", 3)
     timestamp = message_array[0]
     command_type = message_array[1]
     message = message_array[2]
 
     #ip address
     if command_type == "C":
+        clients.append(message)
 
+    for client in clients:
+        greetings = "you are connected"
+        greetings_encoded = greetings.encode("utf-8")
+        sock.sendto(greetings_encoded, (client, CLIENT_UDP_PORT))
 
     test_file.append(msg)
