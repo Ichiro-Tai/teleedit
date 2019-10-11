ce32e0ac40c5cdfde97198a22e3e4943831e7f6c
yunqian4 <yunqian4@illinois.edu>
2019-10-11 16:47:42 -0500

    udp demo

--- host.py
+++ host.py
@@ -15,7 +15,7 @@ print("Your Computer IP Address is:" + ip_addr)
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
-sock.bind((UDP_IP, UDP_PORT))
+sock.bind((ip_addr, UDP_PORT))
 
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
