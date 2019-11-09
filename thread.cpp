1d9937968a8fce4e2349bc4fd22ac0f40ac37908
Billy Li <zl20@linux-07.ews.illinois.edu>
2019-11-09 15:58:38 -0600

    added read, write, readdir

--- thread.cpp
+++ thread.cpp
@@ -6,6 +6,7 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <string>
+#include <dirent.h>
 #include "thread.h"
 #include "include/rapidjson/document.h"
 #include "include/rapidjson/writer.h"
@@ -43,10 +44,10 @@ string read_current_file(string filename) {
   return to_return;
 }
 
-string read_current_file(string filename, size_t size, size_t offset){
-  string allContents = read_current_file(filename);
-  return string.substr(offset, size);
-}
+// string read_current_file(string filename, size_t size, size_t offset){
+//   string allContents = read_current_file(filename);
+//   return string.substr(offset, size);
+// }
 
 string append_to_current_file(string filename, string to_append) {
   ofstream file;
@@ -73,6 +74,46 @@ string insert_to_current_file(string filename, string to_insert, int pos) {
   file << file_contents.c_str();
   file.close();
   return "successfully inserted " + to_insert + " to file: " + filename + " at position: " + std::to_string(pos) + "\n";
+}
+
+string read_file(string filename, int bytes_to_read, int offset) {
+  ifstream file;
+  file.open(filename.c_str(), ios::binary);
+  if (!file) {
+    return "cannot open file: " + filename + "\n";
+  }
+
+  file.seekg(offset, ios::end);
+  std::string data(bytes_to_read, '\0');
+  file.read(&data[0], bytes_to_read);
+  return data;
+}
+
+string write_file(string filename, string data, int offset) {
+  fstream file;
+  file.open(filename.c_str(), ios::binary);
+  if (!file) {
+    return "cannot open file: " + filename + "\n";
+  }
+
+  file.seekp(offset, ios::beg);
+  file.write(data.c_str(), data.length());
+  return "successfully written to " + filename + '\n';
+}
+
+string read_dir(string dir_name) {
+  struct dirent* de;
+  DIR* dr = opendir(dir_name.c_str());
+  if (!dr) {
+    return "cannot open directory: " + dir_name + "\n";
+  }
+
+  string directory_contents;
+  while ((de == readdir(dr)) != NULL) {
+    directory_contents += de->d_name;
+    directory_contents += "\n";
+  }
+  return directory_contents;
 }
 //
 // string delete_at_current_file(string filename, int pos, int length) {
@@ -108,13 +149,25 @@ void* handleConnection(void* sock) {
       send(socket, greetings.c_str(), greetings.length(), 0);
     } else if (connection_type.compare("append") == 0) {
       string to_append = json_msg["data"].GetString();
-      string feedback = append_to_current_file("test_file", to_append);
+      string path = json_msg["path"].GetString();
+      string feedback = append_to_current_file(path, to_append);
       send(socket, feedback.c_str(), feedback.length(), 0);
     } else if (connection_type.compare("read") == 0){
-      string filename = json_msg["path"].GetString();
-      size_t size = json_msg["size"]
+      string path = json_msg["path"].GetString();
+      size_t size = json_msg["size"].GetInt();
+      size_t offset = json_msg["offset"].GetInt();
+      string feedback = read_file(path, size, offset);
+      send(socket, feedback.c_str(), feedback.length(), 0);
     } else if (connection_type.compare("write") == 0){
-
+      string path = json_msg["path"].GetString();
+      string data = json_msg["data"].GetString();
+      size_t offset = json_msg["offset"].GetInt();
+      string feedback = write_file(path, data, offset);
+      send(socket, feedback.c_str(), feedback.length(), 0);
+    } else if (connection_type.compare("readdir") == 0){
+      string path = json_msg["path"].GetString();
+      string feedback = read_dir(path);
+      send(socket, feedback.c_str(), feedback.length(), 0);
     }
     //
     // if (type.GetString() == "disconnect") {
